cf7605b1a5887d00cf75675fe28a2a297f6f6c30
Helena Charles <helsz@hotmail.co.uk>
2022-02-22 22:35:34 +0000

    wip7

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 // import logo from './logo.svg';x
 import './App.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Clock from 'react-live-clock';
-// import useWindowSize from './useWindowSize';
-// import Confetti from 'react-confetti'
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 
 function App() {
-  // const [heSaidYes, setHeSaidYes] = useState(false);
-  // const { width, height } = useWindowSize()
+  const [heSaidYes, setHeSaidYes] = useState(false);
+  const { width, height } = useWindowSize();
+
+  const buttonFunction = () => {
+    setHeSaidYes(true)
+    // window.alert('CONGRATULATIONS YOU HAVE MADE THE BEST DECISION EVER')
+  };
 
   return (
     <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center' }} className="App">
@@ -28,19 +33,30 @@ function App() {
         </a>
       </header> */}
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', }}>
-        <Clock style={{ color: '#228B22', fontSize: 56 }} format={'HH:mm:ss'} ticking={true} timezone={'GB'} />
+        {/* <Clock style={{ color: '#228B22', fontSize: 56 }} format={'HH:mm:ss'} ticking={true} timezone={'GB'} />
         <h1 style={{ color: '#228B22', fontSize: 56 }}>SAHR SOLAR</h1>
         <p style={{ color: '#228B22', fontSize: 48 }}>Will you be my boyfriend?</p>
-        <button style={{ border: 'none', borderRadius: 10, padding: '12px 24px', color: '#fff', backgroundColor: '#228B22', fontSize: 48 }} onClick={() => window.alert('CONGRATULATIONS YOU HAVE MADE THE BEST DECISION EVER')}>Yes</button>
+        <button style={{ border: 'none', borderRadius: 10, padding: '12px 24px', color: '#fff', backgroundColor: '#228B22', fontSize: 48 }} onClick={buttonFunction}>Yes</button> */}
+        {/* <button style={{ border: 'none', borderRadius: 10, padding: '12px 24px', color: '#fff', backgroundColor: '#228B22', fontSize: 48 }} onClick={() => setHeSaidYes(true)}>Yes</button> */}
 
-        {/* {
-          heSaidYes && (
-            <Confetti
-              width={width}
-              height={height}
-            />
+        {
+          heSaidYes ? (
+            <>
+              <Confetti
+                width={width}
+                height={height}
+              />
+              <h1 style={{ color: '#228B22', fontSize: 56 }}>CONGRATULATIONS YOU HAVE MADE THE BEST DECISION EVER</h1>
+            </>
+          ) : (
+            <>
+              <Clock style={{ color: '#228B22', fontSize: 56 }} format={'HH:mm:ss'} ticking={true} timezone={'GB'} />
+              <h1 style={{ color: '#228B22', fontSize: 56 }}>SAHR SOLAR</h1>
+              <p style={{ color: '#228B22', fontSize: 48 }}>Will you be my boyfriend?</p>
+              <button style={{ border: 'none', borderRadius: 10, padding: '12px 24px', color: '#fff', backgroundColor: '#228B22', fontSize: 48 }} onClick={buttonFunction}>Yes</button>
+            </>
           )
-        } */}
+        }
 
 
 
