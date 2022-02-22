7a749b3590226fc543d2c1a69fc147230b0ba304
Helena Charles <helsz@hotmail.co.uk>
2022-02-22 23:00:05 +0000

    wip12

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';x
 import './App.css';
 
 import React, { useState } from 'react';
@@ -11,34 +10,13 @@ function App() {
   const [heSaidYes, setHeSaidYes] = useState(false);
   const { width, height } = useWindowSize();
 
-  const buttonFunction = () => {
+  const onYesButtonClick = () => {
     setHeSaidYes(true)
-    // window.alert('CONGRATULATIONS YOU HAVE MADE THE BEST DECISION EVER')
   };
 
   return (
     <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center' }} className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', }}>
-        {/* <Clock style={{ color: '#228B22', fontSize: 56 }} format={'HH:mm:ss'} ticking={true} timezone={'GB'} />
-        <h1 style={{ color: '#228B22', fontSize: 56 }}>SAHR SOLAR</h1>
-        <p style={{ color: '#228B22', fontSize: 48 }}>Will you be my boyfriend?</p>
-        <button style={{ border: 'none', borderRadius: 10, padding: '12px 24px', color: '#fff', backgroundColor: '#228B22', fontSize: 48 }} onClick={buttonFunction}>Yes</button> */}
-        {/* <button style={{ border: 'none', borderRadius: 10, padding: '12px 24px', color: '#fff', backgroundColor: '#228B22', fontSize: 48 }} onClick={() => setHeSaidYes(true)}>Yes</button> */}
-
         {
           heSaidYes ? (
             <>
@@ -54,13 +32,10 @@ function App() {
               <Clock style={{ color: '#228B22', fontSize: 48 }} format={'HH:mm:ss'} ticking={true} timezone={'GB'} />
               <h1 style={{ color: '#228B22', fontSize: 48 }}>SAHR SOLAR</h1>
               <p style={{ color: '#228B22', fontSize: 36 }}>Will you be my boyfriend?</p>
-              <button style={{ border: 'none', borderRadius: 10, padding: '12px 24px', color: '#fff', backgroundColor: '#228B22', fontSize: 36 }} onClick={buttonFunction}>Yes</button>
+              <button style={{ border: 'none', borderRadius: 10, padding: '12px 24px', color: '#fff', backgroundColor: '#228B22', fontSize: 36 }} onClick={onYesButtonClick}>Yes</button>
             </>
           )
         }
-
-
-
       </div>
     </div>
   );
