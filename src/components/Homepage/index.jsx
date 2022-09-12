d4093ba88f370ec9b38c53a000a183b29697f979
Helena Charles <helsz@hotmail.co.uk>
2022-09-12 15:57:51 +0100

    Change clock on homepage + change button font

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { use100vh } from 'react-div-100vh'
-import Clock from 'react-live-clock';
+// import Clock from 'react-live-clock';
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
 
@@ -33,10 +33,11 @@ const Homepage = () => {
               {/* uncomment this to test link to new page */}
               <Link to="/6-months-anniversary">6 Months Anniversary</Link>
               <Link to="/27th-birthday">Happy 27th Birthday Sahr</Link>
-              <p style={{ color: '#228B22', fontSize: 48 }}>22 / 02 / 2022</p>
-              <Clock style={{ color: '#228B22', fontSize: 48 }} format={'HH:mm:ss'} ticking={true} timezone={'GB'} />
-              <h1 style={{ color: '#228B22', fontSize: 48 }}>SAHR SOLAR</h1>
-              <p style={{ color: '#228B22', fontSize: 36 }}>Will you be my boyfriend?</p>
+              <p style={{ color: '#228B22', fontSize: 48, margin: 16 }}>22 / 02 / 2022</p>
+              <p style={{ color: '#228B22', fontSize: 48, margin: 16 }}>22:22</p>
+              {/* <Clock style={{ color: '#228B22', fontSize: 48 }} format={'HH:mm:ss'} ticking={true} timezone={'GB'} /> */}
+              <h1 style={{ color: '#228B22', fontSize: 48, margin: 16 }}>SAHR SOLAR</h1>
+              <p style={{ color: '#228B22', fontSize: 36, margin: 16 }}>Will you be my boyfriend?</p>
               <button style={{ border: 'none', borderRadius: 10, padding: '12px 24px', color: '#fff', backgroundColor: '#228B22', fontSize: 36 }} onClick={onYesButtonClick}>Yes</button>
             </>
           )
